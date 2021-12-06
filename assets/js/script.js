7f39dac0c22ef1da318fc9dd2910596c63880200
Zsvob <svobodazachary@gmail.com>
2021-12-06 16:55:24 -0500

    rrefine add new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,24 @@ var formEl = document.querySelector("#task-form");
 var tasktodoEL = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    //create list item
     var listItemEL = document.createElement("li");
     listItemEL.className = "task-item";
-    listItemEL.textContent = "This is a new Task.";
+
+    //creat div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEL.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasktodoEL.appendChild(listItemEL); 
 }
 
